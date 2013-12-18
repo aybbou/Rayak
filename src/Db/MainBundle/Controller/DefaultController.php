1d980f24b4cde09f0841f0bf85765a27461d6531
Ayyoub BOUMYA <aybbou@gmail.com>
2013-12-18 11:33:20 +0000

    Added some features.

--- src/Db/MainBundle/Controller/DefaultController.php
+++ src/Db/MainBundle/Controller/DefaultController.php
@@ -4,7 +4,6 @@ namespace Db\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-
 use Db\CreatorBundle\Form\PatentType;
 use Db\CreatorBundle\Entity\Patent;
 
@@ -13,25 +12,41 @@ class DefaultController extends Controller {
     public function indexAction() {
         return $this->render('DbMainBundle:Default:index.html.twig');
     }
-    
-    public function countryInventorsAction(){
+
+    public function collabInventorsAction() {
+        $data = array();
         $em = $this->getDoctrine()->getManager();
-        
-        $countryCode= $_GET['countryCode'];
+
+        $inventors = $em->getRepository('DbCreatorBundle:Inventor')->findAll();
+
+        foreach ($inventors as $inventor) {
+            $data[$inventor->getFullName()] = $inventor->getPatents()->count();
+        }
+
+        arsort($data);
+
+        $response = new JsonResponse($data);
+        return $response;
+    }
+
+    public function countryInventorsAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $countryCode = $_GET['countryCode'];
         $country = $em->getRepository('DbCreatorBundle:Country')->find($countryCode);
-        
-        if(!$country){
-            $error = array('error'=>'Ce pays n\'a pas d\'inventeurs!');
+
+        if (!$country) {
+            $error = array('error' => 'Ce pays n\'a pas d\'inventeurs!');
             return new JsonResponse($error);
         }
-        
+
         $inventors = $country->getInventors();
-        $data =array();
-        
-        foreach($inventors as $inventor){
-            $data[]=array('name'=>$inventor->getFullName());
+        $data = array();
+
+        foreach ($inventors as $inventor) {
+            $data[] = array('name' => $inventor->getFullName());
         }
-        
+
         $response = new JsonResponse($data);
         return $response;
     }
@@ -54,9 +69,55 @@ class DefaultController extends Controller {
         $em = $this->getDoctrine()->getManager();
         $patents = $em->getRepository('DbCreatorBundle:Patent')->findAll();
 
-        $data = $this->get('db.extractor')->getKeywordsFromPatents($patents);
+        $n = null;
+        if (isset($_GET['n'])) {
+            $n = (int) $_GET['n'];
+        }
+
+        $data = $this->get('db.extractor')->getKeywordsFromPatents($patents, $n);
 
         $response = new JsonResponse($data);
+        return $response;
+    }
+
+    public function inventorsAction() {
+        $em = $this->getDoctrine()->getManager();
+        
+        if(isset($_GET['c'])){
+            $c = $_GET['c'];
+            $inventors = $em->getRepository('DbCreatorBundle:Inventor')->findBy(array('country'=>$c));
+        }else{
+            $inventors = $em->getRepository('DbCreatorBundle:Inventor')->findAll();
+        }
+        
+
+        $data = array();
+
+        foreach ($inventors as $inventor) {
+            $data[$inventor->getFullName()] = $inventor->getPatents()->count();
+        }
+
+        arsort($data);
+        
+        $break=false;
+        if(isset($_GET['n'])){
+            $n = $_GET['n'];
+            $break=true;
+        }
+
+        $d = array();
+        $c = 1;
+        foreach ($data as $key => $count) {
+            $d[] = array('name' => $key, 'count' => $count);
+            if($break){
+                if ($c == $n) {
+                    break;
+                }
+            }
+            $c++;
+        }
+
+        $response = new JsonResponse($d);
         return $response;
     }
 
--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,145 @@
+function showInventorsBar(data, divId) {
+    var cats = new Array();
+    var d = new Array();
+    var l = data.length;
+    for (var i = 0; i < l; i++) {
+        cats.push(data[i].name);
+        d.push(data[i].count);
+    }
 
+    $(divId).highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Top 10 inventeurs'
+        },
+        xAxis: {
+            categories: cats
+        },
+        yAxis: {
+            min: 0,
+            allowDecimals:false,
+            title: {
+                text: 'Nombre de brevets'
+            }
+        },
+        legend: {
+          enabled: false  
+        },
+        tooltip: {
+            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+            pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y}</b></td></tr>',
+            footerFormat: '</table>',
+            shared: true,
+            useHTML: true
+        },
+        plotOptions: {
+            column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+            }
+        },
+        series: [{
+                name: 'Brevets ',
+                data: d
+            }]
+    });
+}
+
+function showKeywordsCloud(data) {
+    //var data =[['foo',123],['bar',145]];
+    WordCloud($('#keywordsCloud')[0], {list: data});
+}
+
+function showKeywordsPieChart(data) {
+    $('#keywordsPiechart').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false
+        },
+        title: {
+            text: 'Top 10 mots-clés'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    color: '#000000',
+                    connectorColor: '#000000',
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                }
+            }
+        },
+        series: [{
+                type: 'pie',
+                name: 'Pourcentage ',
+                data: data
+            }]
+    });
+}
+
+function createMap(data) {
+    jQuery('#vmap').vectorMap({
+        values: data,
+        map: 'world_en',
+        backgroundColor: '#a5bfdd',
+        borderColor: '#818181',
+        borderOpacity: 0.25,
+        borderWidth: 1,
+        color: '#f4f3f0',
+        enableZoom: true,
+        hoverColor: '#c9dfaf',
+        hoverOpacity: null,
+        normalizeFunction: 'linear',
+        scaleColors: ['#b6d6ff', '#005ace'],
+        selectedColor: '#c9dfaf',
+        selectedRegion: null,
+        showTooltip: true,
+        onRegionClick: function(element, code, region)
+        {
+            inventors(element, code, region);
+        }
+    });
+}
+
+function showEvolution(data) {
+    $('#charts').highcharts({
+        chart: {
+            type: 'spline'
+        },
+        title: {
+            text: 'Evolution du nombre de brevets publiés'
+        },
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {// don't display the dummy year
+                month: '%e. %b',
+                year: '%b'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Nombre de brevets'
+            },
+            min: 0
+        },
+        tooltip: {
+            formatter: function() {
+                return '<b>' + this.series.name + '</b><br/>' +
+                        Highcharts.dateFormat('%e. %b', this.x) + ': ' + this.y + ' brevets';
+            }
+        },
+        series: [{
+                name: 'Brevets publiés 15/09/2013 - 15/11/2013',
+                data: data
+            }]
+    });
+}
