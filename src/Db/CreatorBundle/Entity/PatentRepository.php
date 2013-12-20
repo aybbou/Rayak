f490a89da1411cbaffa338bb201442efc2f6747c
Younes Sherlock <younesagma@gmail.com>
2013-12-20 20:40:18 +0000

    The function countPatentsByPubDate() - that counts the patents according to the publication date - is added

--- src/Db/CreatorBundle/Entity/PatentRepository.php
+++ src/Db/CreatorBundle/Entity/PatentRepository.php
@@ -11,4 +11,15 @@ class PatentRepository extends EntityRepository{
                 ->orderBy('p.publicationDate','ASC')
             ->getQuery()->getResult();
     }
+
+    public function countPatentsByPubDate()
+    {
+        return  $this
+                    ->createQueryBuilder('p')
+                    ->select('count(p.id)')
+                    ->addSelect('p.publicationDate')
+                    ->groupBy('p.publicationDate')
+                    ->orderBy('p.publicationDate','ASC')
+                    ->getQuery()->getResult();
+    }
 }
