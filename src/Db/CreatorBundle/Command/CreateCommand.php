d0abbdeb7766a6f4c89009ead52ea71c093954b9
Younes Sherlock <younesagma@gmail.com>
2013-12-21 23:02:09 +0000

    Fixed conflict

--- src/Db/CreatorBundle/Command/CreateCommand.php
+++ src/Db/CreatorBundle/Command/CreateCommand.php
@@ -26,10 +26,10 @@ class CreateCommand extends ContainerAwareCommand {
         $output->writeln('Preparing the SQL query for FreePatentsOnline...');
 
         $creator = $this->getContainer()->get('db_creator.creator');
-<<<<<<< HEAD
+
         $creator->setXmlFilePath('/home/occulti/Bureau/result.xml');
         $creator->createDb();
-=======
+
         $creator->setXmlFilePath('/home/aybbou/Bureau/result.xml');
         //$creator->createDb();
         
@@ -37,7 +37,7 @@ class CreateCommand extends ContainerAwareCommand {
         $lensCreator = $this->getContainer()->get('db_lens.creator');
         $lensCreator->setXmlFilePath('/home/aybbou/Bureau/results.xml');
         //$lensCreator->createDb();
->>>>>>> 03013a3bc582467faf366ca8631770ff9d33bb6f
+        
 
         $output->writeln('Done! Have a nice day !');
     }
