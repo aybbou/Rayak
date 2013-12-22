c93e53fdf127874c8772db6522914b79c63a8ce1
Ayyoub BOUMYA <aybbou@gmail.com>
2013-12-22 17:52:49 +0000

    The command take now the files path as an argument

--- src/Db/CreatorBundle/Command/CreateCommand.php
+++ src/Db/CreatorBundle/Command/CreateCommand.php
@@ -3,11 +3,8 @@
 namespace Db\CreatorBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\ArrayInput;
 
 /**
  * Description of CreateCommand
@@ -18,61 +15,64 @@ class CreateCommand extends ContainerAwareCommand {
 
     protected function configure() {
         $this->setName('db:create')
-                ->setDescription('Creates the database.');
+                ->setDescription('Creates the database.')
+                ->addArgument('dbname')
+                ->addArgument('filePath');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $this->prepareDb($output);
-        
-        $output->writeln('Preparing the SQL query for FreePatentsOnline...');
-        $creator = $this->getContainer()->get('db_creator.creator');
-        $creator->setXmlFilePath('/home/aybbou/Bureau/result.xml');
-        $creator->setConfig(array(
-            'bundle'=>'Creator',
-            'patent'=>'patent',
-            'idTag'=>true,
-            'title'=>'title',
-            'abstract'=>'abstract',
-            'pubDate'=>'publication_date',
-            'filDate'=>'filing_date',
-            'inventor'=>'inventor',
-            'name'=>'name',
-            'country'=>'country',
-            'applicant'=>'assignee'
-        ));
-        $creator->createDb();
-        
-        $output->writeln('Preparing the SQL query for PatentLens...');
-        $lensCreator = $this->getContainer()->get('db_creator.creator');
-        $lensCreator->setXmlFilePath('/home/aybbou/Bureau/results.xml');
-        $lensCreator->setConfig(array(
-            'bundle'=>'Lens',
-            'patent'=>'patent',
-            'idTag'=>false,
-            'title'=>'Title',
-            'abstract'=>'Abstract',
-            'pubDate'=>'PublicationDate',
-            'filDate'=>'FilingDate',
-            'inventor'=>'ApplicantAndInventor',
-            'name'=>'FullName',
-            'country'=>'Country',
-            'applicant'=>'ApplicantAndInventor'
-        ));
-        $lensCreator->createDb();
-        
+
+        $dbname = $input->getArgument('dbname');
+        $filePath = $input->getArgument('filePath');
+        if ($dbname == 'fpo') {
+            $this->createFpoDb($output, $filePath);
+        }
+
+        if ($dbname == 'lens') {
+            $this->createLensDb($output, $filePath);
+        }
 
         $output->writeln('Done! Have a nice day !');
     }
 
-    protected function prepareDb($output) {
-        $drop = $this->getApplication()->find('doctrine:schema:drop');
-        $arguments = array('command' => 'doctrine:schema:drop', '--force' => true);
-        $input = new ArrayInput($arguments);
-        $returnCode = $drop->run($input, $output);
-        $update = $this->getApplication()->find('doctrine:schema:update');
-        $arguments = array('command' => 'doctrine:schema:update', '--force' => true);
-        $input = new ArrayInput($arguments);
-        $returnCode = $update->run($input, $output);
+    public function createLensDb(OutputInterface $output, $filePath) {
+        $output->writeln('Preparing the SQL query for PatentLens...');
+        $lensCreator = $this->getContainer()->get('db_creator.creator');
+        $lensCreator->setXmlFilePath($filePath);
+        $lensCreator->setConfig(array(
+            'bundle' => 'Lens',
+            'patent' => 'patent',
+            'idTag' => false,
+            'title' => 'Title',
+            'abstract' => 'Abstract',
+            'pubDate' => 'PublicationDate',
+            'filDate' => 'FilingDate',
+            'inventor' => 'ApplicantAndInventor',
+            'name' => 'FullName',
+            'country' => 'Country',
+            'applicant' => 'ApplicantAndInventor'
+        ));
+        $lensCreator->createDb();
+    }
+
+    public function createFpoDb(OutputInterface $output, $filePath) {
+        $output->writeln('Preparing the SQL query for FreePatentsOnline...');
+        $creator = $this->getContainer()->get('db_creator.creator');
+        $creator->setXmlFilePath($filePath);
+        $creator->setConfig(array(
+            'bundle' => 'Creator',
+            'patent' => 'patent',
+            'idTag' => true,
+            'title' => 'title',
+            'abstract' => 'abstract',
+            'pubDate' => 'publication_date',
+            'filDate' => 'filing_date',
+            'inventor' => 'inventor',
+            'name' => 'name',
+            'country' => 'country',
+            'applicant' => 'assignee'
+        ));
+        $creator->createDb();
     }
 
 }
