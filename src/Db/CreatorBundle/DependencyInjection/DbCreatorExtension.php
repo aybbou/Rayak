a761a7fbc2361a6ef444f84bb0b91526812f41b0
Younes Sherlock <younesagma@gmail.com>
2013-12-23 11:29:52 +0000

    Deleted comments : sonar

--- src/Db/CreatorBundle/DependencyInjection/DbCreatorExtension.php
+++ src/Db/CreatorBundle/DependencyInjection/DbCreatorExtension.php
@@ -7,16 +7,9 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
+
 class DbCreatorExtension extends Extension
 {
-    /**
-     * {@inheritDoc}
-     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
