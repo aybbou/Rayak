ab744874bbc043669a194dcd177bcf4e0e7411b3
Ayyoub BOUMYA <aybbou@gmail.com>
2013-12-23 23:39:36 +0000

    Fixed a test file

--- src/Db/MainBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Db/MainBundle/Tests/Controller/DefaultControllerTest.php
@@ -12,6 +12,6 @@ class DefaultControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("FPO")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Free Patents Online")')->count() > 0);
     }
 }
